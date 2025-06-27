edb92bdf4155f4607ed6a618e9002e385a9853da
Dvan143 <dvanish445@yandex.ru>
2025-06-27 18:09:16 +0300

    Refactored and tested EmailService Added logger

--- src/main/java/org/example/emailverifier/brokers/MqDto.java
+++ src/main/java/org/example/emailverifier/brokers/MqDto.java
@@ -1,6 +1,11 @@
 package org.example.emailverifier.brokers;
 
-public class MqDto {
+import java.io.Serial;
+import java.io.Serializable;
+
+public class MqDto implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     private final String to;
     private final String secretCode;
 
