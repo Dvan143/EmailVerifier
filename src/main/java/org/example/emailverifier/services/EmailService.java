bd6948129e0399c71e6092f45ffb4fd0cbce7b0e
Dvan143 <dvanish445@yandex.ru>
2025-06-28 15:43:39 +0300

    Small improves

--- src/main/java/org/example/emailverifier/services/EmailService.java
+++ src/main/java/org/example/emailverifier/services/EmailService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-    @Value("${FROM}")
+    @Value("${MAIL_FROM}")
     String from;
     @Autowired
     JavaMailSender sender;
